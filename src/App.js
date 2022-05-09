b1609c8d68c4a98a32f2eecfe6d6e433be040649
Payhsak-t <triparnawinx@gmail.com>
2022-05-09 14:15:52 +0530

    Search bar and Search bar CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import SearchRecipes from './components/Recipes/SearchRecipes';
+import landingImage from './assets/landingPage.jpg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="landing">
+        <img src={landingImage} alt="loading..." />
+      </div>
+      <main className="search">
+        <section>
+          <h1>Recipe</h1>
+          <p>One stop for all your favourite recipes</p>
+        </section>
+        <SearchRecipes />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
